8d6203ac0fdc2ec8a6e2fdebb8ec763ce34dcd7b
felipe.both <felipe.sfreitas14@gmail.com>
2024-01-21 10:25:59 -0300

    Insert solution to day 6 and 7 day chalenger

---
+++ next_prime.py
@@ -0,0 +1,24 @@
+def NextPrime(n):
+    def is_prime(num):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    return False
+                    break
+                else:
+                    return True
+        else:
+            return False
+
+    if is_prime(n):
+        return n
+    else:
+        while True:
+            n += 1
+            if is_prime(n):
+                return n
+
+
+print(NextPrime(12))
+print(NextPrime(24))
+print(NextPrime(11))
